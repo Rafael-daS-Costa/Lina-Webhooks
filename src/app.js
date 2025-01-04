505f62fd69a1ff6277f470f6865fade32a60f953
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2025-01-04 09:37:12 -0300

    feat: creating setup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,74 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const axios = require('axios')
+require('dotenv').config()
 
-app.use(express.static('public'))
 
-app.get('/',(req, res) => {
-    res.send('Hello World')
+app.use(bodyParser.json())
+
+const token = process.env.META_DEV_TOKEN;
+const myToken = process.env.MY_TOKEN
+
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log(`Example webhook listening`)
 })
 
-app.post('/',(req, res) => {
-    res.send('Hello World')
-})
+//to verify the callback
+app.get('/webhook', (req, res) => {
+    let mode = req.query["hub.mode"];
+    let challange = req.query["hub.challange"];
+    let token = req.query["hub.verify_token"];
 
-app.get('/about/:teste', (req, res) => {
-    res.send(`Meu nome é ${req.params.teste}`)
-})
+    if (mode && token) {
 
-app.listen(port, () => {
-    console.log('Example app listening on ${port}')
+        if (mode === "subscribe" && token === myToken ) {
+            res.status(200).send(challange);
+        } else {
+            res.status(403);
+        }
+
+    }
+});
+
+app.post("/webhook", (req, res) => {
+    let body_param = req.body;
+
+    console.log(JSON.stringify(body_param, null, 2))
+
+    if (body_param.object) {
+        if (body_param.entry &&
+            body_param.entry[0].changes[0].value.message &&
+            body_param.entry[0].changes[0].value.message[0]
+        ) {
+            let phon_no_id = body.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = body.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body.entry[0].changes[0].value.messages[0].text.body;
+
+
+            axios({
+                method: "POST",
+                url: "https://graph.facebook.com/v21.0/"+phon_no_id+"/messages?access_token="+token,
+                data: {
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text: {
+                        body: "Hello, TiaBette"
+                    }
+                },
+                headers: {
+                    "Content-Type":"application/json"
+                }
+            })
+
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello this is webhook setup")
 })
