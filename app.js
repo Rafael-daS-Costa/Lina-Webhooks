f48fa107a1e579f33d0e11d764e457b4d2c8cd66
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2025-01-04 15:22:05 -0300

    fix: testing what is breaking

--- app.js
+++ app.js
@@ -38,44 +38,45 @@ app.get("/webhook", (req, res) => {
     // check if the webhook request contains a message
     // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
     const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+    console.log('Message: ', message)
   
     // check if the incoming message contains text
-    if (message?.type === "text") {
-      // extract the business number to send the reply from it
-      const business_phone_number_id =
-        req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+    // if (message?.type === "text") {
+    //   // extract the business number to send the reply from it
+    //   const business_phone_number_id =
+    //     req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
   
-      // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
-      await axios({
-        method: "POST",
-        url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
-        headers: {
-          Authorization: `Bearer ${META_DEV_TOKEN}`,
-        },
-        data: {
-          messaging_product: "whatsapp",
-          to: message.from,
-          text: { body: "Echo: " + message.text.body },
-          context: {
-            message_id: message.id, // shows the message as a reply to the original user message
-          },
-        },
-      });
+    //   // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+    //   await axios({
+    //     method: "POST",
+    //     url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
+    //     headers: {
+    //       Authorization: `Bearer ${META_DEV_TOKEN}`,
+    //     },
+    //     data: {
+    //       messaging_product: "whatsapp",
+    //       to: message.from,
+    //       text: { body: "Echo: " + message.text.body },
+    //       context: {
+    //         message_id: message.id, // shows the message as a reply to the original user message
+    //       },
+    //     },
+    //   });
   
-      // mark incoming message as read
-      await axios({
-        method: "POST",
-        url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
-        headers: {
-          Authorization: `Bearer ${META_DEV_TOKEN}`,
-        },
-        data: {
-          messaging_product: "whatsapp",
-          status: "read",
-          message_id: message.id,
-        },
-      });
-    }
+    //   // mark incoming message as read
+    //   await axios({
+    //     method: "POST",
+    //     url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
+    //     headers: {
+    //       Authorization: `Bearer ${META_DEV_TOKEN}`,
+    //     },
+    //     data: {
+    //       messaging_product: "whatsapp",
+    //       status: "read",
+    //       message_id: message.id,
+    //     },
+    //   });
+    // }
   
     res.sendStatus(200);
   });
