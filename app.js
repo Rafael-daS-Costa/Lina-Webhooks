134396d2c0c9240b23de155a20fc6c6709d1c708
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2025-01-04 11:12:24 -0300

    fixing errors

--- app.js
+++ app.js
@@ -11,68 +11,75 @@ const token = process.env.META_DEV_TOKEN;
 const myToken = process.env.MY_TOKEN
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example webhook listening`)
-})
+app.listen(process.env.PORT,()=>{
+    console.log("webhook is listening");
+});
 
-//to verify the callback
-app.get('/webhook', (req, res) => {
-    let mode = req.query["hub.mode"];
-    let challange = req.query["hub.challenge"];
-    let token = req.query["hub.verify_token"];
+//to verify the callback url from dashboard side - cloud api side
+app.get("/webhook",(req,res)=>{
+   let mode=req.query["hub.mode"];
+   let challange=req.query["hub.challenge"];
+   let token=req.query["hub.verify_token"];
 
-    if (mode && token) {
 
-        if (mode === "subscribe" && token === myToken ) {
+    if(mode && token){
+
+        if(mode==="subscribe" && token===myToken){
             res.status(200).send(challange);
-        } else {
+        }else{
             res.status(403);
         }
 
     }
+
 });
 
-app.post("/webhook", (req, res) => {
-    let body_param = req.body;
+app.post("/webhook",(req,res)=>{ //i want some 
 
-    console.log(JSON.stringify(body_param, null, 2))
+    let body_param=req.body;
 
-    if (body_param.object) {
-        console.log('inside body_param object')
-        if (body_param.entry &&
-            body_param.entry[0].changes[0].value.messages &&
-            body_param.entry[0].changes[0].value.messages[0]
-        ) {
-            let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+    console.log(JSON.stringify(body_param,null,2));
 
-            console.log("phone number "+phon_no_id);
-            console.log("from "+from);
-            console.log("boady param "+msg_body);
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            axios({
-                method: "POST",
-                url: "https://graph.facebook.com/v21.0/"+phon_no_id+"/messages?access_token="+token,
-                data: {
-                    messaging_product: "whatsapp",
-                    to: from,
-                    text: {
-                        body: "Hello, TiaBette"
-                    }
-                },
-                headers: {
-                    "Content-Type":"application/json"
-                }
-            })
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
 
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v21.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Oie, deu certo "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
+            }
+
     }
+
 });
 
-app.get("/", (req, res) => {
-    res.status(200).send("hello this is webhook setup")
-})
+app.get("/",(req,res)=>{
+    res.status(200).send("hello this is webhook setup");
+});
