507318d4ea6676c42d3ca9ff3a43e35e83a061c9
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2025-03-06 21:59:58 -0300

    fix: ensure existence of directory

--- src/services/mediaService/mediaService.js
+++ src/services/mediaService/mediaService.js
@@ -2,9 +2,17 @@ const { saveMedia } = require('../../utils/fileSaver');
 const { getAudioTranscription } = require('../deepgramService/deepgramService');
 const { getMediaUrl, downloadMedia } = require('../facebookService/facebookService');
 const path = require('path');
+const fs = require('fs');
 
-async function getFileAndTranscribe(mediaId){
-  try{
+function ensureDirectoryExistence(filePath) {
+  const dir = path.dirname(filePath);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
+async function getFileAndTranscribe(mediaId) {
+  try {
     console.log('getFileAndTranscribe start');
     // obtém informações do arquivo do facebook
     const mediaInfo = await getMediaUrl(mediaId);
@@ -12,17 +20,28 @@ async function getFileAndTranscribe(mediaId){
     // faz download do arquivo do facebook
     const mediaFile = await downloadMedia(mediaInfo.url);
 
-    // salva o arquivo na pasta files (ela precisa já estar criada)
-    const filePath = path.resolve(__dirname, `../../files/file-${mediaId}.ogg`);
+    // define o caminho do arquivo, utilizando /tmp para evitar problemas no Render
+    const filePath = path.resolve('/tmp', `file-${mediaId}.ogg`);
+    
+    // garante que a pasta existe
+    ensureDirectoryExistence(filePath);
+
+    // salva o arquivo
     await saveMedia(filePath, mediaFile);
+    console.log(`Arquivo salvo em: ${filePath}`);
+
+    // verifica se o arquivo foi realmente salvo
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`Erro: o arquivo não foi salvo corretamente em ${filePath}`);
+    }
 
     // transcrição do arquivo de áudio
     const transcription = await getAudioTranscription(filePath);
 
     console.log('getFileAndTranscribe end');
     return transcription;
-  } catch (e){
-    throw new Error('Erro ao obter e transcrever arquivo: ', e);
+  } catch (e) {
+    throw new Error('Erro ao obter e transcrever arquivo: ' + e.message);
   }
 }
 
