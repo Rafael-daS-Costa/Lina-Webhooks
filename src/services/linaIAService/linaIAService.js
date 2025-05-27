1e93e850e316a13251f7c269da1f4144d152619d
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2025-05-26 21:18:22 -0300

    refactor: change name of application

--- src/services/linaIAService/linaIAService.js
+++ src/services/linaIAService/linaIAService.js
@@ -2,29 +2,28 @@ const { default: axios } = require('axios');
 
 require('dotenv').config();
 
-const { TIABETE_IA_API_KEY, TIABETE_IA_URL } = process.env;
+const { LINA_IA_API_KEY, LINA_IA_URL } = process.env;
 
-const getTiabeteIaMessage = async (message, userId) => {
+const getLinaIaMessage = async (message) => {
   try {
     const response = await axios.post(
-      TIABETE_IA_URL,
+      LINA_IA_URL,
       {
         message,
-        user_id: userId,
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'X-API-KEY': TIABETE_IA_API_KEY,
+          'X-API-KEY': LINA_IA_API_KEY,
         },
       },
     );
 
-    console.log('Tiabete response:', { response: response.data });
+    console.log('LINA response:', { response: response.data });
     return response.data.response;
   } catch (error) {
     console.error('Erro na requisição:', error);
   }
 };
 
-module.exports = { getTiabeteIaMessage };
+module.exports = { getLinaIaMessage };
